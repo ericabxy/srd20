55ebb0550f5621ae2011edd9c55f9be5e1727696
Eric Abides <ericxdu23@gmail.com>
2025-10-16 14:00:57 -0700

    delete unneeded files

--- Makefile
+++
@@ -1,9 +0,0 @@
-DEST = docs/*
-MKDIR = mkdir -p
-SRC = markdown/*
-
-normal: $(SRC)
-	python scripts/format.py $^
-
-clean:
-	$(RM) $(DEST)
--- scripts/filename.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-import sys
-
-import markdown
-
-path = sys.argv[1]
-name = os.path.basename(path).lower().replace('_', '-')
-print((name,))
